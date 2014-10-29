2d9342d0c866e76ef5cb6a9ff5d3e4d32b2e0c02
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2014-10-29 14:32:37 +0800

    0010 printf(stdio.h)/main.c - 新增「輸出無正負號位(unsigned)整數(integer)型態資料」內容
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- 0010 printf(stdio.h)/main.c
+++ 0010 printf(stdio.h)/main.c
@@ -6,5 +6,9 @@ int main(int argc, char * argv[]){ // 圓弧括號內之內容暫且不提
 		printf("Ｃ程式語言世界你好！\n");
 	}
 	
+	/* 輸出無正負號位(unsigned)整數(integer)型態資料 */{
+		printf("3 + 3 = %d\n", 3 + 3);
+	}
+	
 	return EXIT_SUCCESS; // EXIT_SUCCESS 暫且不提
 }
