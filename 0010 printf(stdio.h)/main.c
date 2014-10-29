86d714cc3488301cd4b94f4e7699588cd6b5af2e
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2014-10-29 14:32:07 +0800

    0010 printf(stdio.h)/main.c - 將不同範圍內容括進子視野內
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- 0010 printf(stdio.h)/main.c
+++ 0010 printf(stdio.h)/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h> // 此行暫且不提
 
 int main(int argc, char * argv[]){ // 圓弧括號內之內容暫且不提
-	printf("Ｃ程式語言世界你好！\n");
+	/* 單純輸出字串 */{
+		printf("Ｃ程式語言世界你好！\n");
+	}
+	
 	return EXIT_SUCCESS; // EXIT_SUCCESS 暫且不提
 }
